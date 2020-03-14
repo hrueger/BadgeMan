39e03b8644fece15487c55a0e16f3ea101e81a85
H. Rüger <hannesrueger@gmx.de>
2020-03-14 07:21:25 -1000

    added session expired infobox

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -3,7 +3,7 @@ import { render } from "../utils/utils";
 
 class HomeController {
   public static home = async (req: Request, res: Response) => {
-    res.send(render("home"));
+    res.send(render("home", {noSession : req.query.noSession == undefined ? false : true}));
   }
 }
 
