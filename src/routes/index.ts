67c7639d79b2da98426a640c6d34099875d2caa0
H. Rüger <hannesrueger@gmx.de>
2020-04-15 17:53:28 -1000

    added settings page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import app from "./app";
 import auth from "./auth";
 import home from "./home";
+import { isLoggedin } from "../middlewares/isLoggedin";
+import AppController from "../controllers/AppController";
 
 const routes = Router();
 
 routes.use("/authorization", auth);
 routes.use("/", home);
 routes.use("/app", app);
+routes.get("/settings", [isLoggedin], AppController.settings);
 
 export default routes;
