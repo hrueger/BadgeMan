0de4456d6a016627fafa07046788385e533676aa
H. Rüger <hannesrueger@gmx.de>
2020-04-15 17:53:56 -1000

    prevent seeing home page when logged in

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -3,6 +3,10 @@ import { render } from "../utils/utils";
 
 class HomeController {
   public static home = async (req: Request, res: Response) => {
+    if (req.session.loggedIn) {
+      res.redirect("/app");
+      return;
+    }
     res.send(render("home", {noSession : req.query.noSession == undefined ? false : true}));
   }
 }
