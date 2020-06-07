b95a69edc8d48427150fae8e49762c8ffddf93fc
Hannes Rüger <hannesrueger@gmx.de>
2020-06-07 12:06:16 +0200

    fixed bull queue

--- src/controllers/DependenciesController.ts
+++ src/controllers/DependenciesController.ts
@@ -3,52 +3,54 @@ import * as Bull from "bull";
 import { getRepository } from "typeorm";
 import { User } from "../entity/system/User";
 import { DependencyUpgradeJob } from "../entity/plugins/dependencies/DependencyUpgradeJob";
+import { config } from "../config/config";
+import { setQueues } from "bull-board";
+import * as fs from "fs";
+
+
+const queue = new Bull("upgradeDependencies", `redis://${config.redisUrl}`);
+setQueues(queue);
+queue.process(async (job, done) => {
+    for (let i = 0; i < 101; i++) {
+        job.progress(i);
+        for (let j = 0; j < 10000; j++) { }
+    }
+    done();
+});/*
+queue.on("progress", (p, x) => {
+    console.log(p, x);
+}); */
 
 class DependenciesController {
     public static base = async (req: Request, res: Response): Promise<void> => {
-        const queue = new Bull("upgradeDependencies", "redis://192.168.99.100:6379");
         const jobs = await getRepository(DependencyUpgradeJob).find(
             { where: { user: await getRepository(User).findOne(req.params.userId) } },
         );
-        let html = "";
+        let html = "<a href='/dependencies/create'>Create</a><br><br>Jobs:<br>";
         for (const job of jobs) {
-            html += `${job.jobId}: ${(await queue.getJob(job.jobId)).progress()}    |    Active: ${(await queue.getJob(job.jobId)).isActive()}     |    Waiting: ${(await queue.getJob(job.jobId)).isWaiting()}     |    Failed: ${(await queue.getJob(job.jobId)).isFailed()}<br>`;
-            console.log(await queue.getJob(job.jobId));
+            html += `${job.jobId}: ${await (await queue.getJob(job.jobId))?.progress()}    |    Active: ${await (await queue.getJob(job.jobId))?.isActive()}     |    Waiting: ${await (await queue.getJob(job.jobId))?.isWaiting()}     |    Failed: ${await (await queue.getJob(job.jobId))?.isFailed()}<br>`;
+            // console.log(await queue.getJob(job.jobId));
         }
         res.send(html);
     };
 
     public static create = async (req: Request, res: Response): Promise<void> => {
-        const queue = new Bull("upgradeDependencies", "redis://192.168.99.100:6379");
-        queue.process((j, done) => {
-            console.log("processing job");
-            let i = 0;
-            setTimeout(() => {
-                j.progress(i);
-                i++;
-                if (i > 97) {
-                    done();
-                }
-            }, 100);
-        });
-        queue.on("progress", (p, x) => {
-            console.log(p, x);
-        })
-        const j = await queue.add("myCoolData");
+        const j = await queue.add({ d: "myCoolData" });
         const user = await getRepository(User).findOne(req.params.userId);
         const dj = new DependencyUpgradeJob();
         dj.jobId = j.id.toString();
         dj.user = user;
         await getRepository(DependencyUpgradeJob).save(dj);
         res.redirect("/dependencies");
-        queue.on('completed', (job, result) => {
+        /*
+        queue.on("completed", (job, result) => {
             console.debug(`Job completed with result ${job}`);
         });
 
 
-        queue.on('progress', (job, progress) => {
+        queue.on("progress", (job, progress) => {
             console.debug(`Job progress with result ${job} ${progress}`);
-        });
+        }); */
     }
 }
 
--- src/routes/dependencies.ts
+++ src/routes/dependencies.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { isLoggedin } from "../middlewares/isLoggedin";
 import DependenciesController from "../controllers/DependenciesController";
+import { UI } from "bull-board";
 
 const router = Router();
 
 router.get("/", [isLoggedin], DependenciesController.base);
 router.get("/create", [isLoggedin], DependenciesController.create);
+router.use("/admin", UI);
 
 export default router;
